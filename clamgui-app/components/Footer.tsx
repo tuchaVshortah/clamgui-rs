ec49c646939484fd9abe604834b44763d24eb37b
tuchaVshortah <no email>
2023-07-04 15:51:36 +0600

    Update Footer

--- clamgui-app/components/Footer.tsx
+++ clamgui-app/components/Footer.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
+            <p>Made with love by <Link href={'https://github.com/tuchaVshortah/clamgui-rs'} className='font-bold underline'>tuchaVshortah</Link>❤️</p>
+
+            <div>
+                <Link href="/" className="text-gray-500">
+                Privacy Policy
+                </Link>
+                <Link href="/" className="text-gray-500">
+                Terms & Conditions
+                </Link>
+            </div>
+            </div>
+        </footer>
     )
 }
 
