a827d892dacba727ab292f83cab402a8041f2592
tuchaVshortah <no email>
2023-08-31 16:23:48 +0600

    Pass parameters to the directory dialog

--- clamgui-app/components/scan/forms/UploadForm.tsx
+++ clamgui-app/components/scan/forms/UploadForm.tsx
@@ -1,14 +1,29 @@
 'use client'
 
 import { directoryDialog, fileDialog } from "@/app/lib/dialogs/open"
+import { useState } from "react"
 
 export default function UploadForm() {
+
+    const [fileDialogEnabled, setfileDialogEnabled] = useState(true);
+    const [recursionEnabled, setrecursionEnabled] = useState(false);
+
+    function recursionToggle() {
+        setfileDialogEnabled(!fileDialogEnabled);
+        setrecursionEnabled(!recursionEnabled);
+    }
+
     return (
-        <div className="flex flex-row justify-between px-2">
-            <button className="rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100">Choose files</button>
-            <button className="rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100" onClick={() => directoryDialog()}>Choose Directory</button>
-            <button className="rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100" onClick={() => directoryDialog(true)}>Choose Directories</button>
-            <button className="rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100">Scan</button>
+        <div className="flex flex-col gap-5">
+            <div className="flex flex-row justify-between px-2">
+                <button disabled={fileDialogEnabled} className="px-3 disabled:opacity-50 rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100">Choose files</button>
+                <button className="px-3 rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100" onClick={() => directoryDialog({recursive: recursionEnabled})}>Choose Directory</button>
+                <button className="px-3 rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100" onClick={() => directoryDialog({multiple: true, recursive: recursionEnabled})}>Choose Directories</button>
+                <button className="px-3 rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100">Scan</button>
+            </div>
+            <div className="flex flex-row justify-center">
+            <button className="px-3 rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100" onClick={() => recursionToggle()}>Recursive</button>
+            </div>
         </div>
     )
 }
