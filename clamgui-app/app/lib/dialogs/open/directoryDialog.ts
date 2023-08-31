c6137ba90beba50fc813a27fdf45b7a0bcc58c0a
tuchaVshortah <no email>
2023-08-31 16:30:27 +0600

    Add an interface for the directory dialog

--- clamgui-app/app/lib/dialogs/open/directoryDialog.ts
+++ clamgui-app/app/lib/dialogs/open/directoryDialog.ts
@@ -2,8 +2,14 @@
 
 import { open } from '@tauri-apps/api/dialog';
 import { downloadDir } from '@tauri-apps/api/path';
+import { readDir } from '@tauri-apps/api/fs';
 
-export default async function directoryDialog(multiple: boolean = false) {
+interface DirectoryDialogOptions {
+    multiple?: boolean;
+    recursive?: boolean;
+}
+
+export default async function directoryDialog({multiple, recursive}: DirectoryDialogOptions = {multiple: false, recursive: false}) {
 
     // Open a selection dialog for directories
     const selected = await open({
@@ -19,6 +25,8 @@ export default async function directoryDialog(multiple: boolean = false) {
         console.log('user cancelled');
     } else {
         console.log(selected);
+        const entries = await readDir(selected, { recursive: recursive });
+        console.log('Entries of the selected directory:');
+        console.log(entries);
     }
 }
-
