dd134a4780e1c217fb17d118a4e0fa5e31169680
tuchaVshortah <no email>
2023-08-31 17:05:56 +0600

    extract a common function and use it in single and multiple directory selection handlers

--- clamgui-app/app/lib/dialogs/open/directoryDialog.ts
+++ clamgui-app/app/lib/dialogs/open/directoryDialog.ts
@@ -2,11 +2,29 @@
 
 import { open } from '@tauri-apps/api/dialog';
 import { downloadDir } from '@tauri-apps/api/path';
-import { readDir } from '@tauri-apps/api/fs';
+import { FileEntry, readDir } from '@tauri-apps/api/fs';
 
 interface DirectoryDialogOptions {
     multiple?: boolean;
     recursive?: boolean;
+}
+
+function getPaths(entries: FileEntry[]) : string[] {
+    
+    const paths: string[] = [];
+
+    for (const entry of entries) {
+        if (entry.children) {
+            const children = entry.children;
+            for (const child of children) {
+                paths.push(child.path);
+            }
+        } else {
+            paths.push(entry.path);
+        }
+    }
+
+    return paths;
 }
 
 export default async function directoryDialog({multiple, recursive}: DirectoryDialogOptions = {multiple: false, recursive: false}) {
@@ -20,13 +38,32 @@ export default async function directoryDialog({multiple, recursive}: DirectoryDi
 
 
     if (Array.isArray(selected)) {
+
         console.log(selected);
+
+        const entries: FileEntry[] = [];
+        for (const s of selected) {
+            const sentries = await readDir(s, { recursive: recursive });
+            entries.push(...sentries)
+        }
+
+        console.log('Entries of the selected directories:');
+        console.log(entries);
+
+        console.log('Paths of the selected directory:');
+        console.log(getPaths(entries));
+
     } else if (selected === null) {
         console.log('user cancelled');
     } else {
         console.log(selected);
+
         const entries = await readDir(selected, { recursive: recursive });
         console.log('Entries of the selected directory:');
+
         console.log(entries);
+        console.log('Paths of the selected directory:');
+
+        console.log(getPaths(entries));
     }
 }
