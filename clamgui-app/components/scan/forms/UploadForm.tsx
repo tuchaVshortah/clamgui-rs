04298ff827b2bbeebae315bcd33874db2cf0f42e
tuchaVshortah <no email>
2023-09-08 15:45:42 +0600

    Use a wrapper function to set the returned values from dialogs

--- clamgui-app/components/scan/forms/UploadForm.tsx
+++ clamgui-app/components/scan/forms/UploadForm.tsx
@@ -4,6 +4,26 @@ import { directoryDialog, fileDialog } from "@/app/lib/dialogs/open"
 import { useState } from "react"
 
 export default function UploadForm() {
+    
+    var paths: string[] = [];
+
+    const callDirectoryDialog = async ({multiple = false, recursive = false}) => {
+        const result = await directoryDialog({multiple, recursive});
+        if (result) {
+            paths = result;
+            console.log("PATHS:")
+            console.log(paths)
+        }
+    }
+
+    const callFileDialog = async () => {
+        const result = await fileDialog();
+        if (result) {
+            paths = result;
+            console.log("PATHS:")
+            console.log(paths)
+        }
+    }
 
     const [fileDialogEnabled, setFileDialogEnabled] = useState(true);
     const [recursionEnabled, setRecursionEnabled] = useState(false);
@@ -16,9 +36,9 @@ export default function UploadForm() {
     return (
         <div className="flex flex-col gap-5">
             <div className="flex flex-row justify-between px-2">
-                <button disabled={!fileDialogEnabled} className="px-3 disabled:opacity-50 rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100" onClick={() => fileDialog()}>Choose files</button>
-                <button className="px-3 rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100" onClick={() => directoryDialog({recursive: recursionEnabled})}>Choose Directory</button>
-                <button className="px-3 rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100" onClick={() => directoryDialog({multiple: true, recursive: recursionEnabled})}>Choose Directories</button>
+                <button disabled={!fileDialogEnabled} className="px-3 disabled:opacity-50 rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100" onClick={() => callFileDialog()}>Choose files</button>
+                <button className="px-3 rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100" onClick={() => callDirectoryDialog({recursive: recursionEnabled})}>Choose Directory</button>
+                <button className="px-3 rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100" onClick={() => callDirectoryDialog({multiple: true, recursive: recursionEnabled})}>Choose Directories</button>
                 <button className="px-3 rounded-full font-semibold bg-violet-50 text-violet-700 hover:bg-violet-100">Scan</button>
             </div>
             <div className="flex flex-row justify-center">
